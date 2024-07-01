aa7e82dce58282c0e0b2200ea76db0cb62f2ce09
David Diaz <ctucl-desarrollo@outlook.com>
2024-07-01 17:32:28 -0500

    se cambia host

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     rs232.start()
     gpioManager = gpiosManager()
     try:
-        app.run(debug=True,use_reloader=False)
+        app.run(host='0.0.0.0', port=5000,use_reloader=False)
     finally:
         stop_event.set()
         rs232.join()
