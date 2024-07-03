9d52ede7dc69e93067753a843aebd84aac96eef1
David Diaz <ctucl-desarrollo@outlook.com>
2024-07-03 16:14:02 -0500

    se agrega la parte serial al programa

--- rs232.py
+++ rs232.py
@@ -1,10 +1,10 @@
 import threading
 import time
 import serial
-import random
+
 
 class rs232Comunication(threading.Thread):
-    def __init__(self, stop_event):
+    def __init__(self, stop_event,com):
         super().__init__()
         self.lock = threading.Lock()
         self.stop_event = stop_event
@@ -12,15 +12,29 @@ class rs232Comunication(threading.Thread):
         self.validation = False
         self.n_validations = 0
         self.valor_actual = 0
+        self.ser = serial.Serial(com, 9600, timeout=0.5)
+        self.invalid = 0
     def run(self):
         while not self.stop_event.is_set():
             with self.lock:
-                self.valor_actual = random.randint(0, 40)
-                print(f"Generador: Nuevo valor generado {self.valor_actual}")
-                if self.valor_actual > 35:
-                    self.validation = True
-                else:
-                    self.validation = False
+                if self.ser.in_waiting > 0:
+                    linea = self.ser.readline().decode().strip() 
+                    if linea:
+                        #print(f"Datos crudos recibidos: {linea}")
+                        if linea.startswith('$>') and linea.endswith('#'):
+                            data_string = linea[2:-1]  
+                            if len(data_string) == 64:
+                                if data_string[18] == '6':
+                                    self.data = data_string
+                                    self.validation = True
+                                else:
+                                    self.validation = False
+                                    print("Tarjeta Diferente")
+                            else:
+                                self.validation = False
+                        else:
+                            self.invalid +=1
+                            print("Formato de datos incorrecto")
             time.sleep(1)
     def getData(self):
         return str(self.validation) 
