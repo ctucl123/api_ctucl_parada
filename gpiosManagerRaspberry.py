649fcab695c8aab752917543f38b57c330c12780
David Diaz <ctucl-desarrollo@outlook.com>
2025-04-08 09:21:50 -0500

    🎨 Update to multiples raspberry

--- gpiosManagerRaspberry.py
+++ gpiosManagerRaspberry.py
@@ -1,4 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from gpiozero import Device
 from gpiozero import DigitalOutputDevice, DigitalInputDevice
+
+# Detecta entorno desde .env
+ENV = os.getenv("TARGET", "PI3")
+
+if ENV == "PI5":
+    try:
+        from gpiozero.pins.lgpio import LGPIOFactory
+        Device.pin_factory = LGPIOFactory()
+        print("Usando LGPIOFactory (Raspberry Pi 5)")
+    except Exception as e:
+        from gpiozero.pins.pigpio import PiGPIOFactory
+        Device.pin_factory = PiGPIOFactory()
+        print("Fallo LGPIO, usando PiGPIOFactory como respaldo:", e)
+else:
+    from gpiozero.pins.rpigpio import RPiGPIOFactory
+    Device.pin_factory = RPiGPIOFactory()
+    print("Usando RPiGPIOFactory (Raspberry Pi 3)")
+
 import time
 import random
 
